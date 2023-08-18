a562854783ae2932ff7403e6f732fdc29761a695
Shekoni Ayomide Shakiru <shekonishakiru@Shekonis-MacBook-Pro.local>
2023-08-18 15:38:18 +0100

    bug-fix-v2

--- src/components/TranferModal.jsx
+++ src/components/TranferModal.jsx
@@ -27,7 +27,6 @@ const TranferModal = ({
             const transfer = await contract._transferFrom(account, to, parseInt(item.id))
             // await tokenId.wait()
             console.log(transfer)
-            setTo('')
             onClose()
          } catch (error) {
             alert(error)
@@ -38,7 +37,7 @@ const TranferModal = ({
     <div  className='fixed top-0 left-0 w-full flex justify-center items-center h-screen bg-[#2d2d2d45]'>
       <div className='bg-gray-950 relative z-50 flex flex-col w-[500px] p-8 shadow-lg'>
           <input value={to} onChange={e => setTo(e.target.value)}  placeholder='to' className='mb-3 px-2 bg-transparent border text-white border-gray-700 rounded-lg py-2' />
-          <div onClick={initiateTransfer} className='bg-green-700 text-white text-center py-2 mb-3 cursor-pointer rounded-lg'>MINT</div>
+          <div onClick={initiateTransfer} className='bg-green-700 text-white text-center py-2 mb-3 cursor-pointer rounded-lg'>PROCEED</div>
           <div onClick={onClose} className='bg-red-900 cursor-pointer text-white text-center py-2 rounded-lg'>CANCEL</div>
       </div>
     </div>
